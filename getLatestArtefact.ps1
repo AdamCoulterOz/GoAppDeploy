7a70827163d3ff436d1a3b65b9ec043a6cef15ed
Adam Coulter <adam@Adams-MacBook-Pro.local>
2018-08-19 23:38:02 +1000

    Tested and fixed the artefact get script.

--- getLatestArtefact.ps1
+++ getLatestArtefact.ps1
@@ -1,7 +1,16 @@
+# PowerShell defaults to TLS 1.0, github needs TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 # Get the latest release details in JSON format
 $latestRelease = Invoke-WebRequest https://github.com/vibrato/TechTestApp/releases/latest -Headers @{"Accept"="application/json"}
 
 # Extract the tag_name attribute for use in getting the specific binary needed
 $json = $latestRelease.Content | ConvertFrom-Json
 $latestVersion = $json.tag_name
-$url = "https://github.com/vibrato/TechTestApp/releases/download/$(latestVersion)/echTestApp_$(latestVersion)_win64.zip"
+
+# Build the URL to get the specific win64 version of the app binaries
+$url = "https://github.com/vibrato/TechTestApp/releases/download/$($latestVersion)/TechTestApp_$($latestVersion)_win64.zip"
+
+# Download the file to some local directory
+$output = "C:\Users\adamc\TechTestApp_$($latestVersion)_win64.zip"
+Invoke-WebRequest -Uri $url -OutFile $output
