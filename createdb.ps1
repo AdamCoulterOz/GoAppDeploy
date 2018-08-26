6d633d0d17aa09ef5115a8f8e3b92d09a7849565
Adam Coulter <ads@me.com>
2018-08-26 23:55:20 +1000

    Initial createdb ps script
    Created a db create PowerShell script to install the pg sql odbc driver and then connect to the database server to create the database.

---
+++ createdb.ps1
@@ -0,0 +1,17 @@
+﻿
+choco install psqlodbc -y
+
+$Server = ""
+$Port = "5432"
+$DB = "postgres"
+$Username = ''
+$Password = ''
+$Query = Get-Content 'createdb.sql' -Raw
+
+$conn = New-Object System.Data.Odbc.OdbcConnection
+$conn.ConnectionString = "Driver={PostgreSQL Unicode(x64)};Server=$Server;Port=5432;Database=postgres;Uid=$Username;Pwd=$Password;"
+$cmd = New-object System.Data.Odbc.OdbcCommand($Query,$conn)
+$cmd.CommandTimeout = 60
+$conn.open()
+$cmd.ExecuteNonQuery()
+$conn.close()
