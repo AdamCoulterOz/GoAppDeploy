bb6606e55fa48f107f0d738a2d56904330716691
Adam Coulter <adam@Adams-MBP.lan>
2018-08-31 15:09:52 +1000

    Parameterised createdb and used conn string builder

--- Deploy/createdb.ps1
+++ Deploy/createdb.ps1
@@ -1,15 +1,47 @@
-﻿
+﻿param (
+    [Parameter(Mandatory=$True)]
+    [string]$Server,
+
+    [Parameter(Mandatory=$True)]
+    [string]$Username,
+
+    [Parameter(Mandatory=$True)]
+    [Security.SecureString]$Password,
+
+    [Parameter(Mandatory=$False)]
+    [string]$Port = "5432",
+
+    [Parameter(Mandatory=$False)]
+    [string]$DB = "postgres"
+)
+
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+$webClient = New-Object System.Net.WebClient
+$installScript = $webClient.DownloadString('https://chocolatey.org/install.ps1')
+Invoke-Expression $installScript
+
+# Use Chocolatey to install PostgreSQL ODBC driver
 choco install psqlodbc -y
 
-$Server = ""
-$Port = "5432"
-$DB = "postgres"
-$Username = ''
-$Password = ''
+# Build the connection string
+$builder = New-Object System.Data.Odbc.OdbcConnectionStringBuilder
+$builder.Driver = "PostgreSQL Unicode(x64)"
+$builder.Add("Server", $Server)
+$builder.Add("Port", $Port)
+$builder.Add("Database", $DB)
+$builder.Add("Uid", $Username)
+$builder.Add("Pwd", $(ConvertFrom-SecureString $Password))
+
+# Pass connection string to new connection
+$conn = New-Object System.Data.Odbc.OdbcConnection
+$conn.ConnectionString = $builder.ConnectionString
+#$conn.ConnectionString = "Driver={PostgreSQL Unicode(x64)};Server=$Server;Port=$Port;Database=$DB;Uid=$Username;Pwd=$(ConvertFrom-SecureString $Password);"
+
+# Read query to run from file
 $Query = Get-Content 'createdb.sql' -Raw
 
-$conn = New-Object System.Data.Odbc.OdbcConnection
-$conn.ConnectionString = "Driver={PostgreSQL Unicode(x64)};Server=$Server;Port=5432;Database=postgres;Uid=$Username;Pwd=$Password;"
+# Execute Query
 $cmd = New-object System.Data.Odbc.OdbcCommand($Query,$conn)
 $cmd.CommandTimeout = 60
 $conn.open()
