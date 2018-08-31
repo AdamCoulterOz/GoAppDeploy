ebce3984c06ccab5b760170d0292ae433a8c4310
Adam Coulter <ads@me.com>
2018-08-31 16:51:20 +1000

    Added Drop if DB exists
    wont deal with users connected to it, but neither does the GO app's version of this

--- Deploy/createdb.ps1
+++ Deploy/createdb.ps1
@@ -6,13 +6,13 @@
     [string]$Username,
 
     [Parameter(Mandatory=$True)]
-    [Security.SecureString]$Password,
+    [string]$Password,
 
     [Parameter(Mandatory=$False)]
     [string]$Port = "5432",
 
     [Parameter(Mandatory=$False)]
-    [string]$Database = "postgres"
+    [string]$Database = "app"
 )
 
 # Build the connection string
@@ -20,21 +20,30 @@ $builder = New-Object System.Data.Odbc.OdbcConnectionStringBuilder
 $builder.Driver = "PostgreSQL Unicode(x64)"
 $builder.Add("Server", $Server)
 $builder.Add("Port", $Port)
-$builder.Add("Database", $Database)
+$builder.Add("Database", "postgres")
 $builder.Add("Uid", $Username)
-$builder.Add("Pwd", $(ConvertFrom-SecureString $Password))
+$builder.Add("Pwd", $Password)
 
 # Pass connection string to new connection
 $conn = New-Object System.Data.Odbc.OdbcConnection
 $conn.ConnectionString = $builder.ConnectionString
-#$conn.ConnectionString = "Driver={PostgreSQL Unicode(x64)};Server=$Server;Port=$Port;Database=$DB;Uid=$Username;Pwd=$(ConvertFrom-SecureString $Password);"
 
 # Read query to run from file
-$Query = Get-Content '../config/createdb.sql' -Raw
+$dropQuery = "DROP DATABASE IF EXISTS $Database;"
+
+$createDbQuery = Get-Content '../config/createdb.sql' -Raw
+$createDbQuery = $createDbQuery.Replace("[dbname]",$Database)
+echo $createDbQuery 
 
 # Execute Query
-$cmd = New-object System.Data.Odbc.OdbcCommand($Query,$conn)
-$cmd.CommandTimeout = 60
+$dropCmd = New-object System.Data.Odbc.OdbcCommand($dropQuery,$conn)
+$createCmd = New-object System.Data.Odbc.OdbcCommand($createDbQuery,$conn)
+$createCmd.CommandTimeout = 60
 $conn.Open()
-$cmd.ExecuteNonQuery()
+echo "Attempting DROP IF EXISTS"
+$dropResult = $dropCmd.ExecuteNonQuery();
+echo "DROP Result: $($dropResult.ToString().Trim())"
+echo "Attempting CREATE"
+$createResult = $createCmd.ExecuteNonQuery()
+echo "CREATE Result: $($createResult.ToString().Trim())"
 $conn.Close()
