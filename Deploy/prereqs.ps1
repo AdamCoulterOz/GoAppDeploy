4b217ee6f1c693862c53a1ad43aade2ec13ad01f
Adam Coulter <ads@me.com>
2018-09-02 07:17:11 +1000

    Removed superceeded db create code
    Figured out the ARM template can do this for me.

--- Deploy/prereqs.ps1
+++
@@ -1,8 +0,0 @@
-# Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force
-$webClient = New-Object System.Net.WebClient
-$installScript = $webClient.DownloadString('https://chocolatey.org/install.ps1')
-Invoke-Expression $installScript
-
-# Use Chocolatey to install PostgreSQL ODBC driver
-choco install psqlodbc -y -Force
