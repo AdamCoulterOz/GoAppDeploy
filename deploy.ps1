32f213df20a8a6491c078d1a05d188988e638851
Adam <ads@me.com>
2018-09-02 19:16:43 +1000

    Script Readability and Publishing Binary
    Added command wrapping to make script readable and binary publishing profile to deploy app binary via FTP.

--- deploy.ps1
+++ deploy.ps1
@@ -5,31 +5,38 @@
 #>
 
 param(
- [Parameter(Mandatory=$True,HelpMessage="ID of SUBSCRIPTION to deploy to.")]
+ [Parameter(Mandatory=$True,`
+   HelpMessage="ID of SUBSCRIPTION to deploy to.")]
  [string]
  $subscriptionId,
 
- [Parameter(Mandatory=$True,HelpMessage="Name of RESOURCE GROUP to deploy to; cannot be existing.")]
+ [Parameter(Mandatory=$True,`
+   HelpMessage="Name of RESOURCE GROUP to deploy to; cannot be existing.")]
  [string]
  $resourceGroupName,
 
- [Parameter(Mandatory=$True,HelpMessage="Name of LOCATION to deploy to; specified if resource group is new.")]
+ [Parameter(Mandatory=$True,`
+   HelpMessage="Name of LOCATION to deploy to; specified if resource group is new.")]
  [string]
  $resourceGroupLocation,
 
- [Parameter(Mandatory=$True,HelpMessage="Name for instance of application deployment; must be unique across Azure websites and contain only letters or numbers.")]
+ [Parameter(Mandatory=$True,`
+   HelpMessage="Name for instance of application deployment; must be unique across Azure websites and contain only letters or numbers.")]
  [string]
  $appInstanceName,
 
- [Parameter(Mandatory=$True,HelpMessage="Password to use as default database admin account.")]
+ [Parameter(Mandatory=$True,`
+   HelpMessage="Password to use as default database admin account.")]
  [securestring]
  $dbAdminPassword,
 
- [Parameter(Mandatory=$False,HelpMessage="Path to TEMPLATE file; specified if not template.json in local folder.")]
+ [Parameter(Mandatory=$False,`
+   HelpMessage="Path to TEMPLATE file; specified if not template.json in local folder.")]
  [string]
  $templateFilePath = "./Template/template.json",
 
- [Parameter(Mandatory=$False,HelpMessage="Path to PARAMETERS file; specified if not parameters.json in local folder.")]
+ [Parameter(Mandatory=$False,`
+   HelpMessage="Path to PARAMETERS file; specified if not parameters.json in local folder.")]
  [string]
  $parametersFilePath = "./Template/parameters.json"
 )
@@ -39,10 +46,12 @@ param(
 #******************************************************************************
 $ErrorActionPreference = "Stop"
 
-if((-Not (Test-Path $parametersFilePath)) -Or (-Not (Test-Path $templateFilePath)))
+if((-Not (Test-Path $parametersFilePath)) `
+    -Or (-Not (Test-Path $templateFilePath)))
 {
   # Halt script because parameters and/or templates files cannot be found.
-  Write-Error "Template or Parameters files cannot be found. Halting deployment." -ErrorAction Stop
+  Write-Error "Template or Parameters files cannot be found. Halting deployment." `
+    -ErrorAction Stop
 }
 
 #******************************************************************************
@@ -66,17 +75,67 @@ foreach($resourceProvider in $resourceProviders) {
 }
 
 # Check and Halt if resource group already exists
-$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName `
+                                          -ErrorAction SilentlyContinue
 if($resourceGroup)
 {
   # Halt script because resource group name is already in use.
-  Write-Error "Resource group with name '$resourceGroupName' is already in use. Halting deployment." -ErrorAction Stop
+  Write-Error "Resource group with name '$resourceGroupName' is already in use. `
+                Halting deployment." -ErrorAction Stop
 }
 
 # Create resource group in specified location
-Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'"
-New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+Write-Host "Creating resource group '$resourceGroupName' `
+            in location '$resourceGroupLocation'"
+
+New-AzureRmResourceGroup -Name $resourceGroupName `
+                         -Location $resourceGroupLocation
 
 # Start the deployment
 Write-Host "Starting deployment..."
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath -appInstName $appInstanceName -dbAdminPass $dbAdminPassword
+New-AzureRmResourceGroupDeployment  -ResourceGroupName $resourceGroupName `
+                                    -TemplateFile $templateFilePath `
+                                    -TemplateParameterFile $parametersFilePath `
+                                    -appInstName $appInstanceName `
+                                    -dbAdminPass $dbAdminPassword
+
+#******************************************************************************
+# Deploy Go App Binaries
+#******************************************************************************
+
+Write-Host "Getting latest artefacts from Vibrato github..."
+Invoke-Command "./Helper/getLatestArtefact.ps1"
+
+$appdirectory="./bin"
+
+Copy-Item "./Config/web.config" -Destination $appdirectory
+
+
+Write-Host "Download publishing profile..."
+# Get publishing profile for the web app
+$xml = [xml](Get-AzureRmWebAppPublishingProfile -Name $appInstanceName `
+                                                -ResourceGroupName $resourceGroupName `
+                                                -OutputFile null)
+
+# Extract connection information from publishing profile
+
+$baseXPath = "//publishProfile[@publishMethod=`"FTP`"]"
+
+$username = $xml.SelectNodes("$baseXPath/@userName").value
+$password = $xml.SelectNodes("$baseXPath/@userPWD").value
+$url      = $xml.SelectNodes("$baseXPath/@publishUrl").value
+
+# Upload files recursively
+Set-Location $appdirectory
+$webclient = New-Object -TypeName System.Net.WebClient
+$webclient.Credentials = New-Object System.Net.NetworkCredential($username,$password)
+$files = Get-ChildItem -Path $appdirectory -Recurse | Where-Object{!($_.PSIsContainer)}
+foreach ($file in $files)
+{
+  Write-Host "Uploading file $file"
+    $relativepath = (Resolve-Path -Path $file.FullName -Relative).Replace(".\", "").Replace('\', '/')
+    $uri = New-Object System.Uri("$url/$relativepath")
+    "Uploading to " + $uri.AbsoluteUri
+    $webclient.UploadFile($uri, $file.FullName)
+}
+$webclient.Dispose()
